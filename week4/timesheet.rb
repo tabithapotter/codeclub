dc87e74716436172adbfcc01853d22bc472f1663
phyushin <phyushin@gmail.com>
2016-09-29 20:30:59 +0100

    solution

--- week4/timesheet.rb
+++ week4/timesheet.rb
@@ -1,8 +1,10 @@
 def calculate_hours(mins)
-	mins/100
-end 
+	remainder_mins = mins % 60.to_i
+	hours = (mins - remainder_mins)/60.to_i
+	return "#{hours.to_i} hour(s) and #{remainder_mins.to_i} min(s)"
+end
 
 puts "hackspace timesheet program"
 puts "please enter time in mins"
-puts "that is #{calculate_hours(gets.chomp().to_f)} hours"
+puts "that is #{calculate_hours(gets.chomp().to_f)}"
 gets.chomp
