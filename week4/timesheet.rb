fada45872157645fa6b66bd07003a92f614a6234
phyushin <phyushin@gmail.com>
2016-10-03 20:01:15 +0100

    posted solution

--- week4/timesheet.rb
+++ week4/timesheet.rb
@@ -1,7 +1,19 @@
+@hour = 60
+@day = 24*60
+
 def calculate_hours(mins)
-	remainder_mins = mins % 60.to_i
-	hours = (mins - remainder_mins)/60.to_i
-	return "#{hours.to_i} hour(s) and #{remainder_mins.to_i} min(s)"
+	#puts calculate_days (mins)
+	remainder_mins =mins-(calculate_days(mins) * @day)
+	remainder_hours = (remainder_mins -(remainder_mins) % @hour) /@hour
+	remainder_days = calculate_days(mins)
+	#we've got the total hours and days - the rest will be mins
+	remainder_mins = (mins-(@day*remainder_days)-(remainder_hours*@hour))
+
+	return "#{remainder_days.to_i } days, #{remainder_hours.to_i} hour(s) and #{remainder_mins.to_i} min(s)"
+end
+
+def calculate_days(mins)
+	ret_val =(mins - (mins % @day))/@day
 end
 
 puts "hackspace timesheet program"
