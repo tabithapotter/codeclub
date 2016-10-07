6032765b8033d8174b0d1b72b5da542f97cdeb26
Paul <phyushin@gmail.com>
2016-10-07 12:51:46 +0100

    Week2 Homework (#12)
    added seasons and days using hashes

--- week2/calendar.rb
+++ week2/calendar.rb
@@ -1,5 +1,42 @@
 #1-dimensional array
 months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
-months.each do |month| 
-  puts month
+months.each do |month|
+
+    puts month
 end
+
+seasons = ['Spring','Summer','Autumn','Winter']
+seasons.each do |season|
+
+  puts season
+end
+
+
+
+Jan = {Month:'Jan', Season: 'Winter', Days: '31'};
+Feb = {Month:'Feb', Season: 'Winter', Days: '28'};
+Mar = {Month:'Mar', Season: 'Spring', Days: '31'};
+Apr = {Month:'Apr', Season: 'Spring', Days: '30'};
+May = {Month:'May', Season: 'Spring', Days: '31'};
+Jun = {Month:'Jun', Season: 'Summer', Days: '30'};
+Jul = {Month:'Jul', Season: 'Summer', Days: '31'};
+Aug = {Month:'Aug', Season: 'Summer', Days: '31'};
+Sep = {Month:'Sep', Season: 'Autumn', Days: '30'};
+Oct = {Month:'Oct', Season: 'Autumn', Days: '31'};
+Nov = {Month:'Nov', Season: 'Autumn', Days: '30'};
+Dec = {Month:'Dec', Season: 'Winter', Days: '31'};
+
+
+
+puts Jan
+puts Feb
+puts Mar
+puts Apr
+puts May
+puts Jun
+puts Jul
+puts Aug
+puts Sep
+puts Oct
+puts Nov
+puts Dec
