87e60a42fc422e25cc59220a1a7ef464e804d78f
Paul <phyushin@gmail.com>
2016-10-14 09:30:52 +0100

    Week5 homework (#18)
    * added homework
    
    this homework is for week begginging 17th
    
    * solution to the homework
    
    decoder ring program

--- week5/Homeworkforweek6.rb
+++ week5/Homeworkforweek6.rb
@@ -1,20 +1,29 @@
 #using what we learned about arrays we're going to create a substitution 
 #cipher, to do this we need an array of characters and a "shift_by varaiable"
 
-@alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','x','y','z']
+@alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','x','y','z','1','2','3','4','5','6','7','8','9','0']
 
 @shift_by = 3
 
 def encrypt(clear_text, shift_by)
-	puts "this is the clear text:\m#{clear_text}"
+    cipher_text = Array.new()
+	clear_text.split("").each do |char| 
+		currentPos = @alphabet.index(char)
+		newPos = currentPos + shift_by
+		if (newPos > @alphabet.size())
+			newPos % @alphabet.size()
+		end
+		cipher_text.push(@alphabet[newPos])
+	end
+	return cipher_text.join
 end
 
 def decrypt(cipher_text,shift_by)
-	puts "this is the cipher text:\m#{cipher_text}"
+	return encrypt(cipher_text,shift_by*-1)
 end
 
+puts "Khoor, Ohljk Kdfnvsdfh!\n"
 puts "enter a message"
-encrypt(gets.chomp(),@shift_by)
+puts "encrypted cipher_text:\n#{encrypt(gets.chomp(),@shift_by)}\nenter cipher text"
+puts "decrypted plain text:\n #{decrypt(gets.chomp(),@shift_by)}"
 gets.chomp()
-puts "Khoor, Ohljk Kdfnvsdfh!"
-decrypt(gets.chomp(),@shift_by)
